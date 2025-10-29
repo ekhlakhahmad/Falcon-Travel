5f8205b2332580024515fa5a49602e8f59c89367
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2025-10-29 23:31:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// ===== Slider Function =====
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showNextSlide() {
+	slides[currentSlide].classList.remove("active");
+	currentSlide = (currentSlide + 1) % slides.length;
+	slides[currentSlide].classList.add("active");
+}
+
+setInterval(showNextSlide, 4000);
